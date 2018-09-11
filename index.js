bc5501c2646e553b22cc9bb2b04eacb527c53d48
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-09-11 18:45:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ function showRepositories() {
   //this is set to the XMLHttpRequest object that fired the event
   console.log(this.responseText);
 }
+<<<<<<< HEAD
 
+=======
+ 
+>>>>>>> 12f8274ca24c7f4fd08733615ef63d1cc2a3fdc7
 
 function getRepositories() {
   const req = new XMLHttpRequest();
