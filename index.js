72ade1b770b30b1e676ef75f8b85199b470d098a
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-09-11 18:54:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
+function showCommits() {
+  const commits = JSON.parse(this.responseText);
+  const commitsList = `<ul>${commits
+    .map(
+      commit =>
+        '<li><strong>' +
+        commit.author.login +
+        '</strong> - ' +
+        commit.commit.message +
+        '</li>'
+    )
+    .join('')}</ul>`;
+  document.getElementById('commits').innerHTML = commitsList;
+}
+
+function getCommits(el) {
+  const name = el.dataset.repo; // from show repositories <a> data-repo
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', showCommits);
+  req.open('GET', 'https://api.github.com/repos/octocat/' + name + '/commits');
+  req.send();
+}
+
 function showRepositories() {
-  //this is set to the XMLHttpRequest object that fired the event
   var repos = JSON.parse(this.responseText);
   console.log(repos);
   const repoList = `<ul>${repos
-    .map(r => '<li>' + r.name + '</li>')
+    .map(
+      r =>
+        '<li>' +
+        r.name +
+        ' - <a href="#" data-repo="' +
+        r.name +
+        '" onclick="getCommits(this)">Get Commits</a></li>'
+    )
     .join('')}</ul>`;
   document.getElementById('repositories').innerHTML = repoList;
 }
